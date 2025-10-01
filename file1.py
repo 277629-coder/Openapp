8370ff0af2c7951c8300aca473b34a232cc7fca4
277629-coder <277629@vutbr.cz>
2025-10-01 11:54:36 +0200

    file1.py

--- file1.py
+++ file1.py
@@ -5,8 +5,13 @@ import numpy as np
 st.title("Kružnice s body")
 
 # --------- Vstupy od uživatele ----------
-stred_x = st.number_input("Zadej X souřadnici středu", value=0.0)
-stred_y = st.number_input("Zadej Y souřadnici středu", value=0.0)
+souřadnice = st.text_input("Zadej souřadnice středu X Y (oddělené mezerou)", "0 0")
+try:
+    stred_x, stred_y = map(float, souřadnice.split())
+except:
+    st.error("Zadej dvě čísla oddělená mezerou")
+    st.stop()  # zastaví vykreslení, dokud nejsou validní hodnoty
+
 polomer = st.number_input("Zadej poloměr kružnice", value=1.0)
 pocet_bodu = st.number_input("Zadej počet bodů na kružnici", min_value=1, value=10)
 barva = st.color_picker("Vyber barvu bodů", value="#ff0000")  # barevný picker
@@ -31,4 +36,4 @@ ax.set_aspect("equal", adjustable="box")
 ax.grid(True)
 ax.legend()
 
-st.pyplot(fig)  # <--- místo plt.show()
+st.pyplot(fig)
